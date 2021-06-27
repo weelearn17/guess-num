0af1508eeb8be1818b25635406584a4e57d32ef3
Wenyi Chien <erin1117.cts04@g2.nctu.edu.tw>
2021-06-27 21:42:05 +0800

    add count

--- guess-num.py
+++ guess-num.py
@@ -5,16 +5,19 @@
 
 import random
 r = random.randint(1, 100)
+count = 0 
 while True:
+	count += 1 # count =count + 1 的快寫法
 	num = input('請猜1-100的數字：')
 	num = int(num)
 	if num == r:
 		print('恭喜你猜對了！')
+		print('這是你猜的', count,'次' ) 
 		break
 	elif num > r:
 		print('比答案大喔')
 	elif num < r:
 		print('比答案小喔')
-
+	print('這是你猜的', count,'次' ) #13-19行是一個if架構，寫在最底下才不會重複寫
 
 
